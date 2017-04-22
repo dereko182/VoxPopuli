057c165c73f189f660a938efae84bb9ae77d9818
Arturo Rocha <dereko182@gmail.com>
2017-04-22 15:21:36 -0700

    Proyecto Inicial VoxPopuli

---
+++ VoxPopuliApp/VoxPopuliApp/VoxPopuliApp.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace VoxPopuliApp.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new VoxPopuliApp.App());
+        }
+    }
+}
---
+++ VoxPopuliApp/VoxPopuliApp/VoxPopuliApp/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace VoxPopuliApp.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
